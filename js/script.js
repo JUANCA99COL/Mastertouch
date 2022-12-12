3cf42f32302a1012b85976e7c649bc2fcb821673
JUANCA99COL <jcamilocarorodriguez@gmail.com>
2022-12-11 19:57:44 -0500

    eventos + videos + slideshow + tour navidad

--- js/script.js
+++ js/script.js
@@ -20,36 +20,17 @@ menuBtn.addEventListener('click', () => {
   }
 });
 
-// menu click away fix
-// window.addEventListener('mouseup', function(event){
-//     var box = document .getElementById('box1');
-//     if(event.target = box && event.target.parentNode != box){
-//         box.style.display = 'none';
-//     }
-// });
-
-// slideshow 
-var slideIndex = 0;
-showSlides();
-function showSlides() {
-var i;
-var slides = document.getElementsByClassName("mySlides");
-var dots = document.getElementsByClassName("dot");
-for (i = 0; i < slides.length; i++) {
-  slides[i].style.display = "none";  
-}
-slideIndex++;
-if (slideIndex > slides.length) {slideIndex = 1}    
-for (i = 0; i < dots.length; i++) {
-  dots[i].className = dots[i].className.replace(" active", "");
-}
-slides[slideIndex-1].style.display = "block";  
-dots[slideIndex-1].className += " active";
-setTimeout(showSlides, 3000); // Change image every 2 seconds
-}
-
 // trasnlations 
 function loadGoogleTranslate(){
   new google.translate.TranslateElement (
       "google_element");
+}
+
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";  // IE10
+    elements[i].style.flex = "50%";
+  }
 }
